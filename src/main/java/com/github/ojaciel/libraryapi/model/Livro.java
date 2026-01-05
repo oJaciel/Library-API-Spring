5cb2b8763aa50b00557963342fba3ae269635b51
oJaciel <jacielhbarichellO@gmail.com>
2026-01-05 10:51:19 -0300

    Mapeando as entidades do banco
    Feito o mapeamento das entidades do banco para objetos JPA

---
+++ src/main/java/com/github/ojaciel/libraryapi/model/Livro.java
@@ -0,0 +1,38 @@
+package com.github.ojaciel.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Data
+public class Livro {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    @Column(name = "data_publicacao", nullable = false)
+    private LocalDate dataPublicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", length = 30, nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2, nullable = false)
+    private Double preco;
+
+    @ManyToOne //Muitos livros, para um autor
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+}
