e5790c41f4f8a18a2872799b767366cc6b6352d9
oJaciel <jacielhbarichellO@gmail.com>
2026-01-30 11:04:38 -0300

    Filtrar Autores
    Implementado busca de autores por filtro de nome e nacionalidade.

--- src/main/java/com/github/ojaciel/libraryapi/repository/AutorRepository.java
+++ src/main/java/com/github/ojaciel/libraryapi/repository/AutorRepository.java
@@ -3,7 +3,13 @@ package com.github.ojaciel.libraryapi.repository;
 import com.github.ojaciel.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
+    List<Autor> findByNome(String nome);
+
+    List<Autor> findByNacionalidade(String nacionalidade);
+
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
 }
--- src/main/java/com/github/ojaciel/libraryapi/service/AutorService.java
+++ src/main/java/com/github/ojaciel/libraryapi/service/AutorService.java
@@ -5,6 +5,7 @@ import com.github.ojaciel.libraryapi.model.Autor;
 import com.github.ojaciel.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -27,6 +28,22 @@ public class AutorService {
 
     public void deletar(Autor autor) {
         repository.delete(autor);
+    }
+
+    public List<Autor> pesquisa(String nome, String nacionalidade) {
+        if (nome != null && nacionalidade != null) {
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+
+        if (nome != null) {
+            return repository.findByNome(nome);
+        }
+
+        if (nacionalidade != null) {
+            return repository.findByNacionalidade(nacionalidade);
+        }
+
+        return repository.findAll();
     }
 }
 
