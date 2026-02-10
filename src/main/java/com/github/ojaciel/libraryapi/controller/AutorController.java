6c5237b4ae58aadc4ebc93b9ffca3b2dfaaaf2d2
oJaciel <jacielhbarichellO@gmail.com>
2026-02-10 10:46:11 -0300

    Melhoria na Pesquisa de Autores
    Melhorada a pesquisa de autores, usando a classe Example para a busca.

--- src/main/java/com/github/ojaciel/libraryapi/controller/AutorController.java
+++ src/main/java/com/github/ojaciel/libraryapi/controller/AutorController.java
@@ -78,7 +78,7 @@ public class AutorController {
     public ResponseEntity<List<AutorDTO>> pesquisar(
             @RequestParam(value = "nome", required = false) String nome,
             @RequestParam(value = "nacionalidade", required = false) String nacionalidade) {
-        List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+        List<Autor> resultado = service.pesquisaByExample(nome, nacionalidade);
         List<AutorDTO> lista = resultado.stream().map(autor -> new AutorDTO(autor.getId(),
                 autor.getNome(),
                 autor.getDataNascimento(),
--- src/main/java/com/github/ojaciel/libraryapi/service/AutorService.java
+++ src/main/java/com/github/ojaciel/libraryapi/service/AutorService.java
@@ -6,6 +6,8 @@ import com.github.ojaciel.libraryapi.repository.AutorRepository;
 import com.github.ojaciel.libraryapi.repository.LivroRepository;
 import com.github.ojaciel.libraryapi.validator.AutorValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -58,6 +60,22 @@ public class AutorService {
         }
 
         return repository.findAll();
+    }
+
+    public List<Autor> pesquisaByExample(String nome, String nacionalidade) {
+        var autor = new Autor();
+        autor.setNome(nome);
+        autor.setNacionalidade(nacionalidade);
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreNullValues()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Autor> autorExample = Example.of(autor, matcher);
+
+        return repository.findAll(autorExample);
+
+
     }
 
     //Valida se o autor possui livros cadastrados
